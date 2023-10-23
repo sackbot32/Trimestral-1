62614d8256231f39bd3c9d3df80af5bd8a35f083
sackbot32 <82033635+sackbot32@users.noreply.github.com>
2023-10-23 11:35:28 +0200

    gun added

--- Assets/Script/Shooting.cs
+++ Assets/Script/Shooting.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 [System.Serializable]
 public class WeaponCharacteristic{
+    public string color;
     public int damage;
     public float rate;
     public float spread;
@@ -13,13 +15,24 @@ public class WeaponCharacteristic{
 public class Shooting : MonoBehaviour
 {
     //public int type;
-    public WeaponCharacteristic weaponCharacteristic;
+    public WeaponCharacteristic[] weaponCharacteristic;
+    public int chosenWeapon;
+    public float effectTimer;
     private Ray ray;
     private float lastTimeShot;
     private int layerMask;
+    private LineRenderer gunLine;
+    private ParticleSystem muzzle;
 
     private void Start()
     {
+        gunLine = GetComponent<LineRenderer>(); 
+        gunLine.enabled = false;
+        muzzle = transform.GetChild(0).GetComponent<ParticleSystem>();
+        muzzle.Stop();
+        muzzle.Clear();
+        lastTimeShot = weaponCharacteristic[chosenWeapon].rate;
+        changeColor(weaponCharacteristic[chosenWeapon].color);
         layerMask = LayerMask.GetMask("Shootable", "Enemy");
     }
 
@@ -28,17 +41,91 @@ public class Shooting : MonoBehaviour
     {
         lastTimeShot += Time.deltaTime;
 
-        if (Input.GetButton("Fire1") && lastTimeShot > weaponCharacteristic.rate)
+        if (lastTimeShot > effectTimer)
         {
+            gunLine.enabled = false;
+            muzzle.Stop();
+            muzzle.Clear();
+            gunLine.SetPosition(1, transform.position);
+        }
+
+        if (Input.GetButton("Fire1") && lastTimeShot > weaponCharacteristic[chosenWeapon].rate)
+        {
+            gunLine.enabled = true;
+            gunLine.SetPosition(0, transform.position);
             ray.direction = transform.forward;
             ray.origin = transform.position;
             RaycastHit hit;
-
-            if (Physics.Raycast(ray,out hit, weaponCharacteristic.range,layerMask))
+            lastTimeShot = 0;
+            muzzle.Play();
+            if (Physics.Raycast(ray,out hit, weaponCharacteristic[chosenWeapon].range,layerMask))
             {
-       
+                gunLine.SetPosition(1, hit.point);
+            } else
+            {
+                gunLine.SetPosition(1, transform.position + transform.forward * weaponCharacteristic[chosenWeapon].range);
             }
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) // forward
+        {
+            chosenWeapon += 1;
+            if(chosenWeapon >= weaponCharacteristic.Length) 
+            {
+                chosenWeapon = 0;
+            }
+            changeColor(weaponCharacteristic[chosenWeapon].color);
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // backwards
+        {
+            chosenWeapon -= 1;
+            if (chosenWeapon < 0)
+            {
+                chosenWeapon = 2;
+            }
+            changeColor(weaponCharacteristic[chosenWeapon].color);
+        }
 
+        if (Input.GetKeyDown("1"))
+        {
+            chosenWeapon = 0;
+            changeColor(weaponCharacteristic[chosenWeapon].color);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            chosenWeapon = 1;
+            changeColor(weaponCharacteristic[chosenWeapon].color);
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            chosenWeapon = 2;
+            changeColor(weaponCharacteristic[chosenWeapon].color);
+        }
+
+    }
+
+    private void changeColor(string color)
+    {
+        switch (color)
+        {
+            case "Red":
+                gunLine.startColor = new Color(1,0,0);
+                gunLine.endColor = new Color(1,0,0);
+                muzzle.startColor = new Color(1, 0, 0);
+                break;
+            case "Blue":
+                print("llega a blue ");
+                gunLine.startColor = new Color(0, 0, 1);
+                gunLine.endColor = new Color(0, 0, 1);
+                muzzle.startColor = new Color(0, 0, 1);
+                break;            
+            case "Green":
+                gunLine.startColor = new Color(0, 1, 0);
+                gunLine.endColor = new Color(0, 1, 0);
+                muzzle.startColor =  new Color(0, 1, 0);
+                break;
+
+            default:
+                break;
         }
     }
 }
