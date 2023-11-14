186b8088600d9fc6d888a0d0d16d2e9d9510f969
sackbot32 <82033635+sackbot32@users.noreply.github.com>
2023-11-14 23:05:14 +0100

    GreenEnemyUpdated

--- Assets/Script/GreenEnemy/GreenEnemyBeheavioru.cs
+++ Assets/Script/GreenEnemy/GreenEnemyBeheavioru.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Animations.Rigging;
 
 public class GreenEnemyBeheavioru : MonoBehaviour
 {
     public NavMeshControllerGreen navMeshController;
     public Transform target;
+    private Rigidbody rb;
+    public AimConstraint spine;
+    private Animator animator;
     // Start is called before the first frame update
     private void Awake()
     {
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
     void Start()
     {
@@ -20,12 +27,18 @@ public class GreenEnemyBeheavioru : MonoBehaviour
     {
         if (navMeshController.canWalk && !navMeshController.thereYet())
         {
+            animator.SetBool("Attack",false);
+            spine.constraintActive = false;
+            spine.transform.rotation = new Quaternion(0,0,0,0);
             navMeshController.agent.speed = 3.5f;
             navMeshController.SetTarget(target.position);
         }
         if (navMeshController.thereYet())
         {
-            navMeshController.agent.speed = 0;
+            animator.SetBool("Attack", true);
+            spine.constraintActive = true;
+            spine.rotationOffset = new Vector3(0, 0, 0);
+            rb.velocity = Vector3.zero;
             navMeshController.StopIT();
         }
     }
